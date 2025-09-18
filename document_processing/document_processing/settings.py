bdaa38ca8f725296e6c80704b13745558a545a1c
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-18 23:21:48 +0700

    feat: update docker compose, dockerfile

--- document_processing/document_processing/settings.py
+++ document_processing/document_processing/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'document_processing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'document_processing',
+        'USER': 'tojidev',
+        'PASSWORD': 'toaidev',
+        'HOST': 'db',   
+        'PORT': '5432',
     }
 }
 
