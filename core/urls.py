1b471ad823d6a0e5e275a0070a14ef16ce2d7cc8
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-20 23:19:01 +0700

    feat: implement swagger

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,22 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version="v1",
+        description="API docs cho hệ thống",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@myapi.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +43,9 @@ urlpatterns = [
     
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
+    # Swagger UI
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # ReDoc UI (alternative)
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
