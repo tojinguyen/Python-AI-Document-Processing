9d8dcbb1d557a47682737dc4c848a16adbd10959
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-21 00:18:37 +0700

    feat: implement login api

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, ProfileView
+from .views import RegisterView, ProfileView, LoginView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -13,6 +13,22 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
+#Login
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = User.objects.filter(username=username).first()
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            })
+        return Response({"error": "Invalid credentials"}, status=400)
+
 # Profile
 class ProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
