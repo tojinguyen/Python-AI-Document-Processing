a42d5d5052767fb8e237e4296c7e6a90c275ec72
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-20 16:33:50 +0700

    feat: config database postgresql

--- core/settings.py
+++ core/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',      
+        'USER': 'admin',     
+        'PASSWORD': 'admin',
+        'HOST': 'localhost', 
+        'PORT': '5432',      
     }
 }
 
